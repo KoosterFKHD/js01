efe816998359ade01402d3386fcef77d4b6e68c3
Rabaka Vadim <fsusxxfg@gmail.com>
2025-09-15 14:16:45 +0300

    15092025

--- task-1.js
+++ task-1.js
@@ -1,19 +1,29 @@
-/**
- * 
- * 
- * 
- */
 const price = Number(prompt('Доход'))
 const range = prompt('Промежуток времени (month, day, week)')
 
 // TODO добавить проверки валидности данных
-
+if (!Number.isFinite(price)){
+    throw new Error("Доход введен некорректно")
+}
 const formattedPrice = price.toLocaleString('ru', {
     style: "currency",
     currency: "RUB"
 })
 
 // TODO добавить преобразование range в переведенный аналог
+if ((range !== 'month') && (range !== 'day') && (range !== 'week')){
+    throw new Error("промежуток времени введен некорректно")
+}
 
-const result = `${formattedPrice} в ${range}`
+let formattedRange;
+if (range === 'month'){
+    formattedRange = 'месяц'
+}
+else if (range === 'day'){
+    formattedRange = 'день'
+}
+else if (range === 'week'){
+    formattedRange = 'неделю'
+}
+const result = `${formattedPrice} в ${formattedRange}`
 console.log(result)
--- task-2.js
+++ task-2.js
@@ -1,10 +1,20 @@
-const temp = prompt('Температура')
+const temp = +prompt('Температура')
 const weather = prompt('Погода (clear/cloudy)')
 
 // TODO добавить проверку валидности данных
-
-let result;
+if(!Number.isFinite(temp)){
+    throw new Error("Температура введена некорректно")
+}
+if(weather !== clear || cloudy){
+    throw new Error("Погода введена некорректно")
+}
+let result
 
 // TODO добавить проверки погоды и температуры
-
+if (temp == 25 && weather === 'clear'){
+    result = 'golf'
+}
+else if (temp == 25 && weather === 'clear'){
+    result = 'golf'
+}
 console.log(result)
