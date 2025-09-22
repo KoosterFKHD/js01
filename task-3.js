d280e69f3968b5c009af401832b84c2ee66db2bc
Rabaka Vadim <fsusxxfg@gmail.com>
2025-09-22 13:07:56 +0300

    22092025

--- task-3.js
+++ task-3.js
@@ -1,10 +1,42 @@
 const a = Number(prompt('Число A'))
 const b = Number(prompt('Число B'))
 const sign = prompt('Операция(*/+-)')
-// TODO добавить проверку валидности данных
 
-let result
+    
+    if (isFinite(a)) {
+        throw new Error("!Некорректный ввод числа!")
+    }
+    if (isFinite(b)) {
+        throw new Error("!Некорректный ввод числа!")
+    }
+    if (sign !== '*' && sign !== '/' && sign !== '+' && sign !== '-')
+        {
+            throw new Error("!Некорректный ввод!")
+        }
+    
+    let result
+    
+    switch (sign){
 
-// TODO вычислять result через switch
+        case ('/'):
+            if(b === 0)
+                {
+                    throw new Error("!Делить на 0 нельзя!") 
+                }
+                
+            result = a / b
+            break
+    
+        case ('*'):
+            result = a * b
+            break
+    
+        case ('-'):
+            result = a - b
+            break    
+        case ('+'):
+            result = a + b
+            break        
 
-console.log(result)
+    }
+    console.log(result)
--- task-4.js
+++ task-4.js
@@ -1,7 +1,20 @@
-const word = prompt('Ваше слово')
+const word = prompt("Ваше слово")
+
 const wordLength = word.length //длина
-wordLength ** 0.5 //корень кв
-Math.pow(wordLength, 0.5) //корень кв
-Math.sqrt(wordLength) //корень кв
-const result = 1 //логику дописать
+wordLength ** 0.5 //корень
+Math.pow(wordLength, 0.5) //корень
+Math.sqrt(wordLength) //корень
+
+let result
+
+if(Math.trunc(wordLength ** 0.5) === wordLength ** 0.5)
+{
+    result = 1
+}
+else
+{
+    result = 0
+}
+ //дописать логику []:
+
 console.log(result)
